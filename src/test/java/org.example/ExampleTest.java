b91a2480e2ffdece03e8ed5ae6b96f676b6f07e3
maheshwaramsowdarya@gmail.com <maheshwaramsowdarya@gmail.com>
2023-02-05 16:08:13 -0500

    Initial Commit

---
+++ src/test/java/org.example/ExampleTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class ExampleTest {
+    @Test
+    public void testPostMethod() {
+
+       String payload = "{\"coin1\":\"ABC\"," +
+                "\"coin2\":\"XYZ\"," +
+                "\"coin1Amount\":10," +
+                "\"coin2Amount\":1}";
+
+        given().urlEncodingEnabled(false).
+                contentType("application/json").
+                accept("application/json").
+                body(payload).
+        when().
+                post("https://x8ki-letl-twmt.n7.xano.io/api:gHPd8le5/transaction").
+        then().
+                statusCode(200).
+                body("id", notNullValue()).log().all();
+    }
+
+}
